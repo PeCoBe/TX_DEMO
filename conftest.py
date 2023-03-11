d79665c198b7fa3dde2c115a8d33212544444ebc
PeCoBe <15345409+PeCoBe@users.noreply.github.com>
2023-03-11 17:11:53 -0600

    Added comment

--- conftest.py
+++ conftest.py
@@ -11,6 +11,7 @@ def pytest_configure(config):
     now = datetime.datetime.now()
     config.option.HTML_REPORT_TITLE = "My Test Report"
     config.option.HTML_EXPORT_ENCODING = 'utf-8'
+    # Disabled, causing problems when saving report with Japanese characters.
     # config.option.htmlpath = f"report/report-{now:%Y%m%d-%H%M%S}.html"
     # Copy content of current_version and new_version copy into current_version and new_version
     shutil.copyfile('current_version copy.yaml', 'current_version.yaml')
